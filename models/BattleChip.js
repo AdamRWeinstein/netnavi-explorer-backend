19b87ea13d79a5c9e7c49e90e061139deb666e30
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2024-02-14 14:52:37 -0500

    Updated Battle Chip model

--- models/BattleChip.js
+++ models/BattleChip.js
@@ -3,11 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
 const BattleChipSchema = new Schema({
+    chipNo: String,
     name: String,
-    Description: String,
     element: String,
-    capacity: String,
-    rarity: Number,
     codes: [{
         type: String,
         enum: ['*', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z']
